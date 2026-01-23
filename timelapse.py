8626055256ec25242d73afe932418c9e988d46e9
JohnHageter <jwhageter@mix.wvu.edu>
2026-01-23 18:32:01 -0500

    Move to PySide for ui

--- timelapse.py
+++ timelapse.py
@@ -1,3 +1,9 @@
 from Module.ui.Timelapse.app import TimelapseApp
+import sys
+from PySide6.QtWidgets import QApplication
 
-TimelapseApp().run()
+app = QApplication(sys.argv)
+win = TimelapseApp()
+win.show()
+sys.exit(app.exec())
+
