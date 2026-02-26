c271eb29611104a834113d3a3bfdda6980ff82ea
JohnHageter <jwhageter@mix.wvu.edu>
2026-02-26 09:25:12 -0500

    Working

--- timelapse.py
+++ timelapse.py
@@ -1,11 +1,7 @@
-from cv2 import VideoCapture
-
 from Module.ui.main import TimelapseApp
 import sys
 from PySide6.QtWidgets import QApplication
-import cv2
 
-from Module.io.OpenCVCamera import OpenCVCamera
 
 
 app = QApplication(sys.argv)
