86dc223aa871b364ad3da50bfebf9ada3834b525
JohnHageter <jwhageter@mix.wvu.edu>
2026-04-23 14:44:02 -0400

    Restructure files

--- track_worms.py
+++ track_worms.py
@@ -1,10 +1,4 @@
 from pathlib import Path
-import sys
-
-
-ROOT = Path(__file__).resolve().parents[1]
-sys.path.append(str(ROOT))
-
 from Module.Tracking.Configuration import TrackingParameters
 from Module.protocol.runner import TrackingRunner, AppConfig
 
